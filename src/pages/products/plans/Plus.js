0161d6ffc4fecd2565c35eb3595f6fd1e3aecb02
HuntOreo <hloreair@outlook.com>
2025-09-16 16:28:55 -0400

    Build Product cards

--- src/pages/products/plans/Plus.js
+++ src/pages/products/plans/Plus.js
@@ -1,8 +1,15 @@
 import { createEl, appendEl } from '../../../helper';
+import arrowIcon from '../img/arrow_outward.svg';
 
 const Plus = function () {
   const container = createEl({ classAttr: ['plus', 'card'] });
+  const header = createEl({ tag: 'h2', content: 'Plus' });
+  const price = createEl({ tag: 'p', content: '$2.99/month' });
+  const icon = createEl({ tag: 'img', classAttr: ['icon'] });
+  icon.src = arrowIcon;
 
+  const elements = [header, price, icon]
+  appendEl(container, elements);
   return container;
 }
 
--- src/pages/products/plans/Premium.js
+++ src/pages/products/plans/Premium.js
@@ -1,8 +1,15 @@
 import { createEl, appendEl } from '../../../helper';
+import arrowIcon from '../img/arrow_outward.svg';
 
 const Premium = function () {
   const container = createEl({ classAttr: ['premium', 'card'] });
+  const header = createEl({ tag: 'h2', content: 'Premium' });
+  const price = createEl({ tag: 'p', content: '$6.99/month' });
+  const icon = createEl({ tag: 'img', classAttr: ['icon'] });
+  icon.src = arrowIcon;
 
+  const elements = [header, price, icon]
+  appendEl(container, elements);
   return container;
 }
 
