c5b221c35143ca76167888ed7ad160453a97a7d8
HuntOreo <hloreair@outlook.com>
2025-09-16 16:59:15 -0400

    Build Products page

--- src/pages/products/CallToAction.js
+++ src/pages/products/CallToAction.js
@@ -1,7 +1,34 @@
 import { createEl, appendEl } from '../../helper';
+import Button from '../../parts/Button';
 
 const CallToAction = function () {
   const container = createEl({ classAttr: ['callToAction'] });
+  const contentWrapper = createEl({ tag: 'wrapper' });
+  const header = createEl({
+    tag: 'h2',
+    content: 'Ready to level up your payment process?'
+  });
+  const p = createEl({
+    tag: 'p',
+    classAttr: ['desc'],
+    content: 'usce sit amet volutpat lectus, sit amet imperdiet nisl. Nam efficitur ante quis nunc dictum molestie. ',
+  })
+  const btnsWrapper = createEl({ tag: 'wrapper' });
+  const createAcntBtn = new Button({
+    content: 'Get started now',
+    color: '#2A8E9E',
+  });
+  const linkBtn = new Button({
+    content: 'Learn More',
+    color: '#013347'
+  });
+
+  const content = [header, p];
+  appendEl(contentWrapper, content);
+  const buttons = [createAcntBtn.element, linkBtn.element];
+  appendEl(btnsWrapper, buttons);
+  const elements = [header, contentWrapper, btnsWrapper];
+  appendEl(container, elements);
 
   return container;
 }
