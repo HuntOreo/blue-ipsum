0a4970fa4cdd1870a3852c3159a6da7a220201b8
HuntOreo <hloreair@outlook.com>
2025-09-16 23:30:30 -0400

    Basic styling for preferences on customer page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 import './styles/header.css';
 import './styles/footer.css';
 import './styles/home.css';
+import './styles/customers.css'
 import Header from './parts/Header.js';
 import Home from './pages/Home.js';
 import Footer from './parts/Footer.js';
